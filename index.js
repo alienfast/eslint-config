5509f61e6eb4bf080bc3035ff6c8e1198fcc8fa2
Kevin Ross <kevin.ross@alienfast.com>
2019-10-07 10:41:55 -0500

    stick to what tsc validates

--- index.js
+++ index.js
@@ -59,6 +59,12 @@ module.exports = {
     '@typescript-eslint/no-empty-function': 'off',
     '@typescript-eslint/no-empty-interface': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-misused-promises': [
+      'error',
+      {
+        checksVoidReturn: false,
+      },
+    ],
     '@typescript-eslint/no-non-null-assertion': 'off',
     '@typescript-eslint/no-unnecessary-type-assertion': 'off',
     '@typescript-eslint/no-unused-vars': 'off',
