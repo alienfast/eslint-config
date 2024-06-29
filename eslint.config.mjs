ce2093282c45e49eec8df13380083cc6c0af69fd
Kevin Ross <kevin.ross@alienfast.com>
2024-06-29 13:06:17 -0500

    basic @typescript-eslint runs [skip ci]

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,7 +3,7 @@ import tseslint from 'typescript-eslint'
 // import { fileURLToPath } from "node:url";
 // import js from "@eslint/js";
 // import { FlatCompat } from "@eslint/eslintrc";
-import configs from "./src/index.js";
+import configs from './src/index.js'
 
 // const __filename = fileURLToPath(import.meta.url);
 // const __dirname = path.dirname(__filename);
@@ -14,40 +14,44 @@ import configs from "./src/index.js";
 // });
 
 export default tseslint.config(
-    // {
-    // name: "project-js",
-    // extends: [
-    //     ...configs.js,  
-    // ],
-    
+  // {
+  // name: "project-js",
+  // extends: [
+  //     ...configs.js,
+  // ],
 
-    // languageOptions: {
-    //     // ecmaVersion: 5,
-    //     // sourceType: "script",
+  // languageOptions: {
+  //     // ecmaVersion: 5,
+  //     // sourceType: "script",
 
-    //     parserOptions: {
-    //         // createDefaultProgram: true,
-    //         project: ["./tsconfig.lint.json"],
-    //     },
-    
-    // }},
-{ 
-    name: 'project-json', 
+  //     parserOptions: {
+  //         // createDefaultProgram: true,
+  //         project: ["./tsconfig.lint.json"],
+  //     },
+
+  // }},
+  {
+    name: 'project-json',
     extends: [
-        // ...configs.json, 
-        // ...configs.markdown,
-        ...configs.js,
+      // ...configs.json,
+      // ...configs.markdown,
+      ...configs.js,
     ],
-        languageOptions: {
-        // ecmaVersion: 5,
-        // sourceType: "script",
+    languageOptions: {
+      // ecmaVersion: 5,
+      // sourceType: "script",
 
-        parserOptions: {
-            // createDefaultProgram: true,
-            project: ["./tsconfig.lint.json"],
-        },
-    
-    }
-}
-
-);
+      parserOptions: {
+        // createDefaultProgram: true,
+        project: ['./tsconfig.lint.json'],
+      },
+    },
+    rules: {
+      '@typescript-eslint/no-unsafe-argument': 'off',
+      '@typescript-eslint/no-unsafe-member-access': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+      '@typescript-eslint/no-unsafe-assignment': 'off',
+      '@typescript-eslint/no-unsafe-call': 'off',
+    },
+  },
+)
