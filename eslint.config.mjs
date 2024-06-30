432ac3c4a895347ef9d24bcb79623598d45d6907
Kevin Ross <kevin.ross@alienfast.com>
2024-06-30 12:55:29 -0500

    lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,12 +4,10 @@ import af from './src/index.js'
 
 /**
  * Project eslint configuration.
- * 
+ *
  * View config with `npx @eslint/config-inspector`
  */
-export default tseslint.config(
-  {
-    name: 'project',
-    extends: [...af.configs.recommended, ...af.limits.jsOnly],
-  },
-)
+export default tseslint.config({
+  name: 'project',
+  extends: [...af.configs.recommended, ...af.limits.jsOnly],
+})
