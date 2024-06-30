07a3462ee3962e6c1fa59e3db0476359ff9313b2
Kevin Ross <kevin.ross@alienfast.com>
2024-06-29 19:48:02 -0500

    swap out eslint-plugin-import for eslint-plugin-import-x [skip ci]

--- src/constants.js
+++ src/constants.js
@@ -11,5 +11,6 @@ export const BUILD_IGNORES = [
 
 export const TS_FILES = ['**/*.{ts,tsx,mts,cts}']
 export const JS_FILES = ['**/*.{js,mjs,cjs}']
+export const ALL_JS_FILES = [...JS_FILES, ...TS_FILES]
 export const JSON_FILES = ['**/*.json']
 export const MD_FILES = ['**/*.md']
--- src/legacy.js
+++ src/legacy.js
@@ -1,17 +1,17 @@
 import path from 'node:path'
 import { fileURLToPath } from 'node:url'
-import { fixupPluginRules } from "@eslint/compat";
-import { FlatCompat } from "@eslint/eslintrc";
-import eslintJs from "@eslint/js";
+import { fixupPluginRules } from '@eslint/compat'
+import { FlatCompat } from '@eslint/eslintrc'
+import eslintJs from '@eslint/js'
 // import eslintTs from "typescript-eslint";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 export const compat = new FlatCompat({
   baseDirectory: __dirname,
   recommendedConfig: eslintJs.configs.recommended,
-});
+})
 
 /**
  * @param {string} name the pugin name
@@ -19,11 +19,11 @@ export const compat = new FlatCompat({
  * @returns {import("eslint").ESLint.Plugin}
  */
 export function legacyPlugin(name, alias = name) {
-  const plugin = compat.plugins(name)[0]?.plugins?.[alias];
+  const plugin = compat.plugins(name)[0]?.plugins?.[alias]
 
   if (!plugin) {
-    throw new Error(`Unable to resolve plugin ${name} and/or alias ${alias}`);
+    throw new Error(`Unable to resolve plugin ${name} and/or alias ${alias}`)
   }
 
-  return fixupPluginRules(plugin);
+  return fixupPluginRules(plugin)
 }
