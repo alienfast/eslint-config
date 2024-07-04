9a6b1bb3a50a11c3d8aa5970edacc6a6ff95b849
Kevin Ross <kevin.ross@alienfast.com>
2024-07-04 18:44:06 -0500

    force eslint-plugin-react-hooks up to 5@rc

---
+++ .ncurc.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  packageFile: './package.json',
+  packageManager: 'yarn',
+  // reject: [
+  //   '@apollo/client', // F them for not having an esm release.
+  // ],
+  target: (name) => (name === 'eslint-plugin-react-hooks' ? '@rc' : 'latest'),
+  // workspaces: true,
+  // root: true,
+}
